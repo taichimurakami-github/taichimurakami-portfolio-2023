ef387ece0735c031a87e4834fbdacaebe509b037
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2023-02-08 02:29:41 +0900

    Add pointer circle tracer circle

---
+++ components/views/PointerTraceCircle.tsx
@@ -0,0 +1,19 @@
+import usePointerCd from '@/hooks/usePointerCd';
+import { useState } from 'react';
+
+export default function PointerTraceCircle(props: { radius_px?: number }) {
+  const pointerCd = usePointerCd();
+  const [circleRadius_px, setCircleRadius_px] = useState(100);
+
+  return (
+    <div
+      className="absolute bg-orange-400 rounded-full -translate-x-1/2 -translate-y-1/2 opacity-50"
+      style={{
+        width: circleRadius_px,
+        height: circleRadius_px,
+        left: pointerCd.clientX,
+        top: pointerCd.clientY,
+      }}
+    ></div>
+  );
+}
---
+++ hooks/usePointerCd.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+export default function usePointerCd(samplingRate_ms = 100) {
+  const [pointerCd, setPointerCd] = useState({
+    screenX: -1,
+    screenY: -1,
+    clientX: -1,
+    clientY: -1,
+  });
+  const prevSampled = useRef(0);
+
+  const receiveMouseMoveEvent = useCallback((e: MouseEvent) => {
+    if (Date.now() - prevSampled.current < samplingRate_ms) {
+      return;
+    }
+    setPointerCd({
+      screenX: e.screenX,
+      screenY: e.screenY,
+      clientX: e.clientX,
+      clientY: e.clientY,
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', receiveMouseMoveEvent);
+  }, []);
+
+  return pointerCd;
+}
