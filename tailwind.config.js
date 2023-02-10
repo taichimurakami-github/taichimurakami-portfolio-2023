2191b2b6138ec02d23b85261cc08a936253b607e
Taichi Murakami <taichi.murakami.s5@dc.tohoku.ac.jp>
2023-02-10 22:07:23 +0900

    Add 'white' color config, and convert " to '

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
-      "dark-gray-1": "#333333",
-      "dark-gray-2": "#595959",
-      "dark-gray-3": "#7F7F7F",
-      "emerald-1": "#4DC0B2",
+      'dark-gray-1': '#333333',
+      'dark-gray-2': '#595959',
+      'dark-gray-3': '#7F7F7F',
+      'emerald-1': '#4DC0B2',
+      white: '#FFF',
     },
     extend: {},
   },
