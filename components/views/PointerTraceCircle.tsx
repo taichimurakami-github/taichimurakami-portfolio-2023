ebd7a296dbef8340a13ab3fda6b105dbbd1d384a
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2023-02-13 02:47:08 +0900

    Change circle color when pointer is on clickable element

--- components/views/PointerTraceCircle.tsx
+++ components/views/PointerTraceCircle.tsx
@@ -1,19 +1,24 @@
+import useDetectPointerOn from '@/hooks/useDetectPointerOn';
 import usePointerCd from '@/hooks/usePointerCd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function PointerTraceCircle(props: { radius_px?: number }) {
   const pointerCd = usePointerCd();
   const [circleRadius_px, setCircleRadius_px] = useState(100);
+  const pointerOnClickableElement = useDetectPointerOn('class', [
+    'cursor-pointer',
+  ]);
 
   return (
     <div
-      className="absolute bg-orange-400 rounded-full -translate-x-1/2 -translate-y-1/2 opacity-50"
+      className="absolute rounded-full -translate-x-1/2 -translate-y-1/2 opacity-30 z-0"
       style={{
         width: circleRadius_px,
         height: circleRadius_px,
         left: pointerCd.clientX,
         top: pointerCd.clientY,
         visibility: pointerCd.clientX > 0 ? 'visible' : 'hidden',
+        background: pointerOnClickableElement ? 'orange' : 'lightgray',
       }}
     ></div>
   );
