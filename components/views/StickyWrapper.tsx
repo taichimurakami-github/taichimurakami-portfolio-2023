db6528fb2510ef3a7a8a2f4aa262bbb96a589ab4
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2023-02-13 19:58:59 +0900

    Make it force defining sticky position

--- components/views/StickyWrapper.tsx
+++ components/views/StickyWrapper.tsx
@@ -2,10 +2,18 @@ import { PropsWithChildren } from 'react';
 
 export default function StickyWrapper(
   props: PropsWithChildren<{
+    stickyPosition: {
+      top?: string;
+      bottom?: string;
+      left?: string;
+      right?: string;
+    };
     class?: string;
   }>
 ) {
   return (
-    <div className={`sticky top-0 ${props.class ?? ''}`}>{props.children}</div>
+    <div className={`sticky ${props.class ?? ''}`} style={props.shrinkPosition}>
+      {props.children}
+    </div>
   );
 }
