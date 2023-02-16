5f7aaec9834dd5873444c9fa2d293ba61fa5542b
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2023-02-16 22:48:55 +0900

    Update to use both in About and Works section

--- components/views/TableOfContent.tsx
+++ components/views/TableOfContent.tsx
@@ -1,11 +1,5 @@
 import { NextFont } from '@next/font';
-import React, {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import animations from '@/styles/animations.module.scss';
 
 function IndexBar(props: {
@@ -23,15 +17,15 @@ function IndexBar(props: {
       className="relative bg-white"
       style={{
         width: props.barWidth ?? '5px',
-        height: props.barHeight ?? '150px',
+        height: props.barHeight,
       }}
       onClick={props.onHandleBarClick}
     >
       <div
         className={`absolute rounded-full bg-emerald-1 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer ${animations['animate-toc-index-pointer']}`}
         style={{
-          width: props.pointerSize ?? '20px',
-          height: props.pointerSize ?? '20px',
+          width: props.pointerSize ?? '25px',
+          height: props.pointerSize ?? '25px',
           top: props.pointerTop ?? '0px',
           visibility: props.pointerTop ? 'visible' : 'hidden',
         }}
@@ -41,28 +35,52 @@ function IndexBar(props: {
   );
 }
 
-export default function AboutSectionToC(props: {
+export default function SectionToCContainer(props: {
+  id: string;
   contents: JSX.Element[];
   font: NextFont;
+  tocDisplaySide: 'left' | 'right';
+  onHandleActiveContentChange?: (
+    nextActiveId: number,
+    nowActiveId: number
+  ) => void;
   wrapperClass?: string;
-  indexContentClass?: string;
+  contentGap?: number;
+  contentContainerClass?: string;
+  contentContainerActiveClass?: string;
+  barWidth?: string;
+  barHeight?: string;
+  pointerSize?: string;
 }) {
   const [activeId, setActiveId] = useState<null | number>(null);
   const indexContentSample = useRef<HTMLLIElement>(null);
 
   const getIndexPointerTop = () => {
-    if (indexContentSample.current && activeId !== null) {
-      // const indexContentTop = indexContentSample.current.clientTop;
-      const indexContentHeight = indexContentSample.current
-        ? indexContentSample.current.clientHeight
-        : 0;
-
-      const pointerTop_px =
-        indexContentHeight * activeId + indexContentHeight / 2;
-      return pointerTop_px + 'px';
+    if (!indexContentSample.current || activeId === null) {
+      return '0px';
     }
 
-    return '0px';
+    const indexContentHeight = indexContentSample.current
+      ? indexContentSample.current.clientHeight
+      : 0;
+
+    const gap = props?.contentGap ?? 0;
+    const pointerTop_px =
+      indexContentHeight * activeId + indexContentHeight / 2 + gap * activeId;
+
+    return pointerTop_px + 'px';
+  };
+
+  const getBarHeight = () => {
+    if (!indexContentSample.current) {
+      return undefined;
+    }
+
+    const h_gaps = (props.contents.length - 1) * (props.contentGap ?? 0);
+    const h_contents =
+      props.contents.length * indexContentSample.current.clientHeight;
+
+    return h_gaps + h_contents + 'px';
   };
 
   useEffect(() => {
@@ -72,21 +90,37 @@ export default function AboutSectionToC(props: {
   }, []);
 
   return (
-    <div className={`flex gap-4 ${props.wrapperClass ?? ''}`}>
-      <IndexBar pointerTop={getIndexPointerTop()} />
-      <ul className={`text-white ${props.font.className}`}>
+    <div
+      className={`flex gap-4 ${props.wrapperClass ?? ''}`}
+      style={{
+        flexDirection: props.tocDisplaySide === 'left' ? 'row' : 'row-reverse',
+      }}
+    >
+      <ul
+        className={`w-full h-full grid text-white ${props.font.className}`}
+        style={{
+          gap: props.contentGap ?? '0',
+        }}
+      >
         {props.contents.map((v, i) => {
           return (
             <li
-              key={`about_section_toc_${i}`}
+              key={`${props.id}_content_container_#${i}`}
               ref={i === 0 ? indexContentSample : undefined}
               className={`
-              flex items-center cursor-pointer
-              ${activeId === i ? 'text-emerald-1' : ''} 
+              relative flex items-center cursor-pointer
+              ${props.contentContainerClass ?? ''}
               ${animations['animate-active-toc-about-section']}
-              ${props.indexContentClass ?? ''}
+              ${activeId === i ? 'text-emerald-1' : ''} 
+              ${
+                activeId === i && props.contentContainerActiveClass
+                  ? props.contentContainerActiveClass
+                  : ''
+              }
               `}
               onClick={(e) => {
+                props?.onHandleActiveContentChange &&
+                  props?.onHandleActiveContentChange(i, activeId ?? 0);
                 setActiveId(i);
               }}
             >
@@ -95,6 +129,12 @@ export default function AboutSectionToC(props: {
           );
         })}
       </ul>
+      <IndexBar
+        pointerTop={getIndexPointerTop()}
+        barWidth={props.barWidth}
+        barHeight={getBarHeight()}
+        pointerSize={props.pointerSize}
+      />
     </div>
   );
 }
